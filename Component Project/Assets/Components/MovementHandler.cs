c8dc9a67af2bb054b6f6b428b2c013087d21823b
ZenoxZX <114158896+ZenoxZX@users.noreply.github.com>
2023-01-06 18:48:28 +0300

    Sync

---
+++ Component Project/Assets/Components/MovementHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementHandler : MonoBehaviour
+{
+    //[SerializeField] Joystick joystick;
+    //[field: SerializeField] public float ForwardSpeed { get; private set; } = 5;
+    //[field: SerializeField] public float TurnSpeed { get; private set; } = 8;
+    //private void Update() => transform.SetPositionAndRotation(Position, Rotation);
+    //private Vector3 Input => new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+    //private Vector3 Position => transform.position += Time.deltaTime * ForwardSpeed * Input;
+    //private Quaternion Rotation => Input != Vector3.zero ? Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Input, Vector3.up), TurnSpeed * Time.deltaTime) : transform.rotation;
+}
